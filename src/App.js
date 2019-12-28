b7d51f6ac27502c6cede9f5078e60d5215e48f94
Taylor Darneille <tmdarneille@gmail.com>
2019-12-27 19:52:15 -0800

    I'm tired

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div id="section03" className="demo">
+      <div id="section03" className="header">
         <h1>Misha Lilac LMP, RYT</h1>
+        <h2>Yoga. Meditation. Massage. Bodywork.</h2>
+        <h3><a href="https://www.thelavenderlioness.com/services-1">Book Now</a></h3>
         <a href="#section04"><span></span></a>
       </div>
        <div id="section04" className="demo">
