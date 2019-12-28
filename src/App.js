b24c22ff7eb60062695556e9b6fb93ce38291fc2
Taylor Darneille <tmdarneille@gmail.com>
2019-12-27 20:05:30 -0800

    need to move around some CSS

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div id="section03" className="header">
+      <div id="section03" className="demo">
         <h1>Misha Lilac LMP, RYT</h1>
         <h2>Yoga. Meditation. Massage. Bodywork.</h2>
         <h3><a href="https://www.thelavenderlioness.com/services-1">Book Now</a></h3>
         <a href="#section04"><span></span></a>
       </div>
        <div id="section04" className="demo">
-        <h1>Scroll Down Button #4</h1>
       </div>
     </div>
 
