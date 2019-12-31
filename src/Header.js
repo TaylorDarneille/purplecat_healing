28eb1607b1cce6d594d73c168aa69da677e7db08
Taylor Darneille <tmdarneille@gmail.com>
2019-12-31 12:02:07 -0800

    cleaned up some CSS

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,9 @@ function Header() {
     <header id="section03" className="section demo">
       <h1>Misha Lilac LMP, RYT</h1>
       <h2>Yoga. Meditation. Massage. Bodywork.</h2>
-      <h3><a href="https://squareup.com/appointments/book/5Z6Y1FC2CPY33">Book Now</a></h3>
+      <div className="book">
+        <h3><a href="https://squareup.com/appointments/book/5Z6Y1FC2CPY33">Book Now</a></h3>
+      </div>
       <a id="scroll" href="#main"><span></span></a>
     </header>
   );
